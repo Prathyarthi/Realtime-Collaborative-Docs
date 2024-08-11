2a9794d1ca7cb7679ed2012c5c4d175a52b1389a
Prathyarthi <prathyarti@gmail.com>
2024-08-11 19:00:22 +0530

    Modify title functionality and get all Documents

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -8,7 +8,7 @@ async function Document({ params: { id } }: SearchParamProps) {
 
     if (!clerkUser) redirect('/sign-in')
 
-    const room = getDocument({
+    const room = await getDocument({
         roomId: id,
         userId: clerkUser.emailAddresses[0].emailAddress
     })
